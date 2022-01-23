03b1944d6ab527e465cfd7f227717b22b266cbc2
JJackson510 <jamiej.jj5@gmail.com>
2022-01-23 10:48:52 -0600

    added the questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,67 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt ([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Waht is the name of your project?',
+            validate: nameInput =>{
+                if(nameInput) {
+                    return true;
+                }else{
+                    console.log('Please enter a valid name!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub username',
+            validate: githubInput => {
+                if(githubInput) {
+                    return true;
+                }else{
+                    console.log('Please enter a valid GitHub username!');
+                }
+            }
+        },
+        {
+            type : 'input',
+            name :'about',
+            message: 'Please tell us about your project',
+            validate: aboutInput => {
+                if(aboutInput) {
+                    return true;
+                }else {
+                    console.log('Please give us a description of your project');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'istall',
+            message: 'What are the installation instructions for your project?',
+        },
+        {
+            type: 'input',
+            name: 'emailInput',
+            message: 'Please enter your email address',
+            validate: emailInput => {
+                if(emailInput){
+                    return true;
+                }else{
+                    console.log('Please provide a valid email address.');
+                }
+            }
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
